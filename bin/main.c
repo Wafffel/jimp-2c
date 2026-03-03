32fa39e6cd82ce6e579be432b279cb03c74aabf2
Wafffel <kristoferski.kw@gmail.com>
2026-03-03 18:05:53 +0100

    move to src

--- bin/main.c
+++
@@ -1,6 +0,0 @@
-int main(int argc, char *argv[]) {
-    char help_message[] = "Usage: program [options] <input-file> <output-file>\n"
-                          "Options:\n"
-                          "  -h, --help    Show this help message\n";
-    return 0;
-}
