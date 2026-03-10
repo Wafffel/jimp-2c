1e8a37d5bd824de3eaa887dad9604a6164b26139
Wafffel <kristoferski.kw@gmail.com>
2026-03-10 15:25:31 +0100

    format files

--- src/graph.h
+++ src/graph.h
@@ -2,25 +2,25 @@
 #define GRAPH_H
 
 typedef struct {
-    int id;
-    double x, y;
+  int id;
+  double x, y;
 } Node;
 
 typedef struct {
-    int first_node_index; int second_node_index;
-    double weight;
-    char label[33];
+  int first_node_index;
+  int second_node_index;
+  double weight;
+  char label[33];
 } Edge;
 
 typedef struct {
-    Node* nodes;
-    int nodes_count;
-    Edge* edges;
-    int edges_count;
+  Node *nodes;
+  int nodes_count;
+  Edge *edges;
+  int edges_count;
 } Graph;
 
-
-Graph* load_graph(char path[]);
+Graph *load_graph(char path[]);
 int get_node_index(Graph *graph, int node_id);
 void save_graph_as_binary(Graph *graph, char path[]);
 void save_graph_as_text(Graph *graph, char path[]);
--- src/utils.h
+++ src/utils.h
@@ -2,12 +2,12 @@
 #define UTILS_H
 
 enum {
-	SUCCESS = 0,
-    ARGUMENTS_ERROR = 1,
-	FILE_ERROR = 2,
-	INPUT_FORMAT_ERROR = 3,
-	ALGORITHM_ERROR = 4,
-	MEMORY_ERROR = 5,
+  SUCCESS = 0,
+  ARGUMENTS_ERROR = 1,
+  FILE_ERROR = 2,
+  INPUT_FORMAT_ERROR = 3,
+  ALGORITHM_ERROR = 4,
+  MEMORY_ERROR = 5,
 };
 
 typedef struct List {
@@ -15,7 +15,7 @@ typedef struct List {
   struct List *next;
 } List;
 
-int  list_contains(List *head, int value);
+int list_contains(List *head, int value);
 void list_prepend(List **head, int value);
 void list_free(List *head);
 
