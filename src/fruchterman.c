3cf73e4d80331d22529af28640ecb49ae250e0eb
Wafffel <kristoferski.kw@gmail.com>
2026-03-24 11:43:36 +0100

    fix fruchterman

--- src/fruchterman.c
+++ src/fruchterman.c
@@ -61,43 +61,43 @@ int run_fruchterman(Graph *graph, double initial_temperature,
         }
       }
     }
-  }
-  // Siły przyciągania
-  for (int e = 0; e < liczba_k; e++) {
-    int a = graph->edges[e].first_node_index;
-    int b = graph->edges[e].second_node_index;
-    double waga = graph->edges[e].weight;
-    double dx = graph->nodes[a].x - graph->nodes[b].x;
-    double dy = graph->nodes[a].y - graph->nodes[b].y;
-    double odleglosc = sqrt(dx * dx + dy * dy) + 1e-9;
-    double fa = waga * (odleglosc * odleglosc) / k;
-    sila_x[a] -= (dx / odleglosc) * fa;
-    sila_y[a] -= (dy / odleglosc) * fa;
-    sila_x[b] += (dx / odleglosc) * fa;
-    sila_y[b] += (dy / odleglosc) * fa;
-  }
-  // Aktualizacja pozycji
-  for (int i = 0; i < liczba_w; i++) {
-    double wek_sily = sqrt(sila_x[i] * sila_x[i] + sila_y[i] * sila_y[i]);
-    if (wek_sily > 0) {
-      double przesuniecie = fmin(wek_sily, temperature);
-      graph->nodes[i].x += (sila_x[i] / wek_sily) * przesuniecie;
-      graph->nodes[i].y += (sila_y[i] / wek_sily) * przesuniecie;
+    // Siły przyciągania
+    for (int e = 0; e < liczba_k; e++) {
+      int a = graph->edges[e].first_node_index;
+      int b = graph->edges[e].second_node_index;
+      double waga = graph->edges[e].weight;
+      double dx = graph->nodes[a].x - graph->nodes[b].x;
+      double dy = graph->nodes[a].y - graph->nodes[b].y;
+      double odleglosc = sqrt(dx * dx + dy * dy) + 1e-9;
+      double fa = waga * (odleglosc * odleglosc) / k;
+      sila_x[a] -= (dx / odleglosc) * fa;
+      sila_y[a] -= (dy / odleglosc) * fa;
+      sila_x[b] += (dx / odleglosc) * fa;
+      sila_y[b] += (dy / odleglosc) * fa;
     }
-    // Ograniczenie do obszaru
-    if (graph->nodes[i].x < 0.0)
-      graph->nodes[i].x = 0.0;
-    if (graph->nodes[i].x > size)
-      graph->nodes[i].x = size;
-    if (graph->nodes[i].y < 0.0)
-      graph->nodes[i].y = 0.0;
-    if (graph->nodes[i].y > size)
-      graph->nodes[i].y = size;
-  }
-  // Chłodzenie
-  temperature -= initial_temperature / max_iterations;
-  if (temperature < 0.0) {
-    temperature = 0.0;
+    // Aktualizacja pozycji
+    for (int i = 0; i < liczba_w; i++) {
+      double wek_sily = sqrt(sila_x[i] * sila_x[i] + sila_y[i] * sila_y[i]);
+      if (wek_sily > 0) {
+        double przesuniecie = fmin(wek_sily, temperature);
+        graph->nodes[i].x += (sila_x[i] / wek_sily) * przesuniecie;
+        graph->nodes[i].y += (sila_y[i] / wek_sily) * przesuniecie;
+      }
+      // Ograniczenie do obszaru
+      if (graph->nodes[i].x < 0.0)
+        graph->nodes[i].x = 0.0;
+      if (graph->nodes[i].x > size)
+        graph->nodes[i].x = size;
+      if (graph->nodes[i].y < 0.0)
+        graph->nodes[i].y = 0.0;
+      if (graph->nodes[i].y > size)
+        graph->nodes[i].y = size;
+    }
+    // Chłodzenie
+    temperature -= initial_temperature / max_iterations;
+    if (temperature < 0.0) {
+      temperature = 0.0;
+    }
   }
   free(sila_x);
   free(sila_y);
